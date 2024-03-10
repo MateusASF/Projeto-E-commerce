d30a1cf3362834650a6889ec8f6e3642a2292858
Mateus Augusto dos Santos Fonseca <mateus.asfonseca@gmail.com>
2024-03-09 22:01:33 -0300

    função de adicionar múltiplos endereços e cartões

--- Front/src/app/cliente/components/cadastro/cadastro.component.ts
+++ Front/src/app/cliente/components/cadastro/cadastro.component.ts
@@ -1,67 +1,93 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
+
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  styleUrls: ['./cadastro.component.css'],
 })
 export class CadastroComponent {
-
   form!: FormGroup;
 
   ngOnInit() {
     this.form = new FormGroup({
       nome: new FormControl(''),
       nascimento: new FormControl(''),
-      generoForm: new FormGroup({
-        genero: new FormControl('')
-      }),
+      genero: new FormControl(''),
       cpf: new FormControl(''),
-      telefone: new FormControl(''),
-      tipoTelefoneForm: new FormGroup({
-        tipoTelefone: new FormControl('')
-      }),
+      telefone: new FormArray([
+        new FormGroup({
+          numeroTelefone: new FormControl(''),
+          tipoTelefone: new FormControl('')
+        })
+      ]),
       senha: new FormControl(''),
       email: new FormControl(''),
+      enderecos: new FormArray([]),
+      cartoes: new FormArray([])
+    });
+
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      const formData = this.form.value;
+      const json = JSON.stringify(formData);
+      console.log(json);
+      // fetch('http://localhost:3009/cadastro', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json'
+      //   },
+      //   body: json
+      // })
+      // .then(response => response.json())
+      // .then(data => {
+      //   console.log('Response:', data);
+      //   // Handle the response data here
+      // })
+      // .catch(error => {
+      //   console.error('Error:', error);
+      //   // Handle any errors here
+      // });
+    }
+  }
+
+  adicionarEndereco() {
+    const enderecoFormGroup = new FormGroup({
       logradouro: new FormControl(''),
-      logradouroForm: new FormGroup({
-        tipoLogradouro: new FormControl('')
-      }),
+      tipoLogradouro: new FormControl(''),
       numero: new FormControl(''),
       bairro: new FormControl(''),
       cidade: new FormControl(''),
       estado: new FormControl(''),
       pais: new FormControl(''),
       cep: new FormControl(''),
-      residenciaForm: new FormGroup({
-        tipoResidencia: new FormControl('')
-      }),
-      observacoes: new FormControl(''),
+      tipoResidencia: new FormControl(''),
+      observacoes: new FormControl('')
     });
+
+    (this.form.get('enderecos') as FormArray).push(enderecoFormGroup);
   }
 
-  onSubmit() {
-      if (this.form.valid) {
-        const formData = this.form.value;
-        const json = JSON.stringify(formData);
+  adicionarCartao() {
+    const cartaoFormGroup = new FormGroup({
+      numeroCartao: new FormControl(''),
+      nomeCliente: new FormControl(''),
+      bandeira: new FormControl(''),
+      cvv: new FormControl('')
+    });
 
-        // fetch('http://localhost:3009/cadastro', {
-        //   method: 'POST',
-        //   headers: {
-        //     'Content-Type': 'application/json'
-        //   },
-        //   body: json
-        // })
-        // .then(response => response.json())
-        // .then(data => {
-        //   console.log('Response:', data);
-        //   // Handle the response data here
-        // })
-        // .catch(error => {
-        //   console.error('Error:', error);
-        //   // Handle any errors here
-        // });
-    }
+    (this.form.get('cartoes') as FormArray).push(cartaoFormGroup);
   }
+
+  get cartoesControls() {
+    return this.form.get('cartoes') ? (this.form.get('cartoes') as FormArray).controls : [];
+  }
+
+  get enderecosControls() {
+    return this.form.get('enderecos') ? (this.form.get('enderecos') as FormArray).controls : [];
+  }
+
 }
